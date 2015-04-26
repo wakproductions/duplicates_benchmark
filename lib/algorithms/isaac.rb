c5a202f970a84a47ce7526f64059eafb10e9874b
Isaac Sanders <isaac@isaacbfsanders.com>
2015-04-26 14:52:52 -0400

    Update isaac.rb

--- lib/algorithms/isaac.rb
+++ lib/algorithms/isaac.rb
@@ -1,7 +1,7 @@
 module DuplicatesBenchmark
   module Isaac
     def self.duplicates(a)
-      a.group_by {|a| a }.inject({}) do |hash, key, val|
+      a.group_by {|a| a }.inject({}) do |hash, (key, val)|
         hash[key] = val.count if val.count > 1
         hash
       end
